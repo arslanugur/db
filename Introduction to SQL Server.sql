368e758e2e6b229d2c6a209c4b5fdd9e688d5e13
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-25 08:46:33 +0300

    Update Introduction to SQL Server.sql

--- Introduction to SQL Server.sql
+++ Introduction to SQL Server.sql
@@ -1,5 +1,4 @@
 # 1. SELECTion Box
-
 # 1.2 Simple selections
 It’s time to begin writing your own queries! In this first coding exercise, you will use SELECT statements to retrieve columns from a database table. You’ll be working with the eurovision table, which contains data relating to individual country performance at the Eurovision Song Contest from 1998 to 2012.
 After selecting columns, you’ll also practice renaming columns, and limiting the number of rows returned.
