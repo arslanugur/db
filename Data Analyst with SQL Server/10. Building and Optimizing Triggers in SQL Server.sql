cbef7017d4fa6c85d6d1dceafe296ed4a58dda76
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-26 17:08:08 +0300

    Create 10. Building and Optimizing Triggers in SQL Server.sql

---
+++ Data Analyst with SQL Server/10. Building and Optimizing Triggers in SQL Server.sql
@@ -0,0 +1,71 @@
+1. Introduction to Triggers
+An introduction to the basic concepts of SQL Server triggers. Create your first trigger using T-SQL code. 
+Learn how triggers are used and what alternatives exist.
+
+1.1. Introduction
+1.2. Types of trigger
+1.3. Creating your first trigger
+1.4. Practicing creating triggers
+1.5. How DML triggers are used
+1.6. When to use triggers
+1.7. Creating a trigger to keep track of data changes
+1.8. Trigger alternatives
+1.9. Triggers vs. stored procedures
+1.10. Triggers vs. computed columns
+
+
+
+2. Classification of Triggers
+Learn about the different types of SQL Server triggers: AFTER triggers (DML), INSTEAD OF triggers (DML), DDL triggers, and logon triggers.
+
+2.1. AFTER triggers (DML)
+2.2. Tracking retired products
+2.3. The TrackRetiredProducts trigger in action
+2.4. Practicing with AFTER triggers
+2.5. INSTEAD OF triggers (DML)
+2.6. Preventing changes to orders
+2.7. PreventOrdersUpdate in action
+2.8. Creating the PreventNewDiscounts trigger
+2.9. DDL triggers
+2.10. Tracking table changes
+2.11. Using FOR in a trigger
+2.12. Preventing table deletion
+2.13. Logon triggers
+2.14. Enhancing database security
+2.15. Defining a logon trigger
+
+
+3. Trigger Limitations and Use Cases
+Find out known limitations of triggers, as well as common use cases for AFTER triggers (DML), INSTEAD OF triggers (DML) and DDL triggers.
+
+3.1. Known limitations of triggers
+3.2. Trigger limitations
+3.3. Creating a report on existing triggers
+3.4. Use cases for AFTER triggers (DML)
+3.5. Keeping a history of row changes
+3.6. Table auditing using triggers
+3.7. Use cases for INSTEAD OF triggers (DML)
+3.8. Preventing changes to Products
+3.9. Checking stock before placing orders
+3.10. Use cases for DDL triggers
+3.11. Database auditing
+3.12. Preventing server changes
+
+
+4. Trigger Optimization and Management
+Learn to delete and modify triggers. Acquaint yourself with the way trigger management is done. 
+Learn how to investigate problematic triggers in practice.
+
+4.1. Deleting and altering triggers
+4.2. Removing unwanted triggers
+4.3. Modifying a trigger's definition
+4.4. Disabling a trigger
+4.5. Re-enabling a disabled trigger
+4.6. Trigger management
+4.7. Managing existing triggers
+4.8. Counting the AFTER triggers
+4.9. Troubleshooting triggers
+4.10. Keeping track of trigger executions
+4.11. Identifying problematic triggers
+4.12. Wrapping up
+
