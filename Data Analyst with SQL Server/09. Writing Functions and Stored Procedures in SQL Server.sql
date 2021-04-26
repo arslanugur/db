8f503a19548e5d9caf5aeae08934c78f8527bc95
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-26 14:35:53 +0300

    Update 09. Writing Functions and Stored Procedures in SQL Server.sql

--- Data Analyst with SQL Server/09. Writing Functions and Stored Procedures in SQL Server.sql
+++ Data Analyst with SQL Server/09. Writing Functions and Stored Procedures in SQL Server.sql
@@ -196,20 +196,241 @@ SELECT DATEADD(month, DATEDIFF(month, 0, GETDATE()), 0)
 This chapter will explain how to create, update, and execute user-defined functions (UDFs). 
 You will learn about the various types of UDFs: scalar, inline, and multi-statement table-valued. 
 
-2.1. Scalar user defined functions
+2.1. Scalar user defined functions - Video
 2.2. What was yesterday?
+Create a function that returns yesterday's date.
+
+Instructions
+Create a function named GetYesterday() with no input parameters that RETURNS a date data type.
+Use GETDATE() and DATEADD() to calculate yesterday's date value.
+
+-- Create GetYesterday()
+CREATE FUNCTION GetYesterday()
+-- Specify return data type
+RETURNS date
+AS
+BEGIN
+-- Calculate yesterday's date value
+RETURN(SELECT DATEADD(day, -1, GETDATE()))
+END 
+
+
 2.3. One in one out
+Create a function named SumRideHrsSingleDay() which returns the total ride time in hours for the @DateParm parameter passed.
+
+Instructions
+Define input parameter of type date - @DateParm and a return data type of numeric.
+Use BEGIN/END keywords.
+In ur SELECT statement, SUM the difference between the StartDate and EndDate of the transactions that have the same StartDate value as the parameter passed.
+Use CAST to compare the date portion of StartDate to the @DateParm.
+
+-- Create SumRideHrsSingleDay
+CREATE FUNCTION SumRideHrsSingleDay (@DateParm date)
+-- Specify return data type
+RETURNS numeric
+AS
+-- Begin
+BEGIN
+RETURN
+-- Add the difference between StartDate and EndDate
+(SELECT SUM(DATEDIFF(second, StartDate, EndDate))/3600
+FROM CapitalBikeShare
+ -- Only include transactions where StartDate = @DateParm
+WHERE CAST(StartDate AS date) = @DateParm)
+-- End
+END
+
+
 2.4. Multiple inputs one output
-2.5. Table valued UDFs
+Often times you will need to pass more than one parameter to a function. 
+Create a function that accepts @StartDateParm and @EndDateParm and returns the total ride hours for all transactions that have a StartDate within the parameter values.
+
+Instructions
+Create a function named SumRideHrsDateRange with @StartDateParm and @EndDateParm as the input parameters of datetime data type.
+Specify the return data type to be numeric.
+Use a select statement to sum the difference between the StartDate and EndDate of the transactions.
+Only include transactions that have a StartDate greater than @StartDateParm and less than @EndDateParm.
+
+-- Create the function
+CREATE FUNCTION SumRideHrsDateRange (@StartDateParm datetime, @EndDateParm datetime)
+-- Specify return data type
+RETURNS numeric
+AS
+BEGIN
+RETURN
+-- Sum the difference between StartDate and EndDate
+(SELECT SUM(DATEDIFF(second, StartDate, EndDate))/3600
+FROM CapitalBikeShare
+-- Include only the relevant transactions
+WHERE StartDate > @StartDateParm and StartDate < @EndDateParm)
+END
+
+
+
+2.5. Table valued UDFs - Video
 2.6. Inline TVF
+Create an inline table value function that 
+returns the number of rides and total ride duration for each StartStation where the StartDate of the ride is equal to the input parameter.
+
+Instructions
+Create a function named SumStationStats that has one input parameter of type datetime - @StartDate - and returns a TABLE data type.
+Calculate the total RideCount using COUNT() and ID.
+Calculate the TotalDuration using SUM() and DURATION.
+Group by StartStation.
+
+-- Create the function
+CREATE FUNCTION SumStationStats(@StartDate AS datetime)
+-- Specify return data type
+RETURNS TABLE
+AS
+RETURN
+SELECT
+	StartStation,
+    -- Use COUNT() to select RideCount
+	COUNT(ID) as RideCount,
+    -- Use SUM() to calculate TotalDuration
+    SUM(DURATION) as TotalDuration
+FROM CapitalBikeShare
+WHERE CAST(StartDate as Date) = @StartDate
+-- Group by StartStation
+GROUP BY StartStation;
+
+
 2.7. Multi statement TVF
-2.8. UDFs in action
+Create a multi statement table value function that returns the trip count 
+and average ride duration for each day for the month & year parameter values passed.
+
+Instructions
+Create a function CountTripAvgDuration() that returns a table variable named @DailyTripStats.
+Declare input parameters @Month and @Year.
+Insert the query results into the @DailyTripStats table variable.
+Use CAST to select and group by StartDate as a date data type.
+
+-- Create the function
+CREATE FUNCTION CountTripAvgDuration (@Month CHAR(2), @Year CHAR(4))
+-- Specify return variable
+RETURNS @DailyTripStats TABLE(
+	TripDate	date,
+	TripCount	int,
+	AvgDuration	numeric)
+AS
+BEGIN
+-- Insert query results into @DailyTripStats
+INSERT @DailyTripStats
+SELECT
+    -- Cast StartDate as a date
+	CAST(StartDate AS date),
+    COUNT(ID),
+    AVG(Duration)
+FROM CapitalBikeShare
+WHERE
+	DATEPART(month, StartDate) = @Month AND
+    DATEPART(year, StartDate) = @Year
+-- Group by StartDate as a date
+GROUP BY CAST(StartDate AS date)
+-- Return
+RETURN
+END
+
+
+2.8. UDFs in action -Video
 2.9. Execute scalar with select
+Previously, you created a scalar function named SumRideHrsDateRange(). 
+Execute that function for the '3/1/2018' through '3/10/2018' date range by passing local date variables.
+
+Instructions
+Create @BeginDate and @EndDate variables of type date with values '3/1/2018' and '3/10/2018'.
+Execute the SumRideHrsDateRange() function by passing @BeginDate and @EndDate variables.
+Include @BeginDate and @EndDate variables in your SELECT statement with the function result.
+
+-- Create @BeginDate
+DECLARE @BeginDate AS date = '3/1/2018'
+-- Create @EndDate
+DECLARE @EndDate AS date = '3/10/2018' 
+SELECT
+  -- Select @BeginDate
+  @BeginDate AS BeginDate,
+  -- Select @EndDate
+  @EndDate AS EndDate,
+  -- Execute SumRideHrsDateRange()
+  dbo.SumRideHrsDateRange(@BeginDate, @EndDate) AS TotalRideHrs
+  
+  
 2.10. EXEC scalar
+You created the SumRideHrsSingleDay function earlier in this chapter. 
+Execute that function using the EXEC keyword and store the result in a local variable.
+
+Instructions
+Create a local numeric variable named @RideHrs.
+Use EXEC to execute the SumRideHrsSingleDay function and pass '3/5/2018' as the input parameter.
+Store the result of the function in @RideHrs variable.
+
+-- Create @RideHrs
+DECLARE @RideHrs AS numeric
+-- Execute SumRideHrsSingleDay function and store the result in @RideHrs
+EXEC @RideHrs = dbo.SumRideHrsSingleDay @DateParm = '3/5/2018' 
+SELECT 
+  'Total Ride Hours for 3/5/2018:', 
+  @RideHrs
+  
+
 2.11. Execute TVF into variable
-2.12. Maintaining user defined functions
+Remember the table value function you created earlier in this chapter named SumStationStats?. 
+It accepts a datetime parameter and returns the ride count and total ride duration for each starting station 
+where the start date matches the input parameter. 
+Execute SumStationStats now and store the results in a table variable.
+
+Instructions
+Create a table variable named @StationStats with columns StartStation, RideCount, and TotalDuration.
+Execute the SumStationStats function and pass '3/15/2018' as the input parameter.
+Use INSERT INTO to populate the @StationStats table variable with the results of the function.
+Select all the records from the table variable.
+
+-- Create @StationStats
+DECLARE @StationStats TABLE(
+	StartStation nvarchar(100), 
+	RideCount int, 
+	TotalDuration numeric)
+-- Populate @StationStats with the results of the function
+INSERT INTO @StationStats
+SELECT TOP 10 *
+-- Execute SumStationStats with 3/15/2018
+FROM dbo.SumStationStats ('3/15/2018') 
+ORDER BY RideCount DESC
+-- Select all the records from @StationStats
+SELECT * 
+FROM @StationStats
+
+
+2.12. Maintaining user defined functions - Video
 2.13. CREATE OR ALTER
+Change the SumStationStats function to enable SCHEMABINDING. 
+Also change the parameter name to @EndDate and compare to EndDate of CapitalBikeShare table.
+
+Instructions
+Use CREATE OR ALTER keywords to update the SumStationStats function.
+Change the parameter name to @EndDate and data type to date.
+Compare the @EndDate to EndDate of the CapitalBikeShare table.
+Enable SCHEMABINDING.
+
+-- Update SumStationStats
+CREATE OR ALTER FUNCTION dbo.SumStationStats(@EndDate AS date)
+-- Enable SCHEMABINDING
+RETURNS TABLE WITH SCHEMABINDING
+AS
+RETURN
+SELECT
+	StartStation,
+    COUNT(ID) AS RideCount,
+    SUM(DURATION) AS TotalDuration
+FROM dbo.CapitalBikeShare
+-- Cast EndDate as date and compare to @EndDate
+WHERE CAST(EndDate AS Date) = @EndDate
+GROUP BY StartStation;
+
 2.14. Best practices
+What makes a function Deterministic?
+---> If it returns the same result given the same parameters passed and database state.
 
 
 
