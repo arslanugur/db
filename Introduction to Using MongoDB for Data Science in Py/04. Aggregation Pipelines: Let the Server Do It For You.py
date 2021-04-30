327deef02c112d7cae168874a2c4f1570e1c4fc1
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-30 20:25:32 +0300

    Create 04. Aggregation Pipelines: Let the Server Do It For You.py

---
+++ Introduction to Using MongoDB for Data Science in Py/04. Aggregation Pipelines: Let the Server Do It For You.py
@@ -0,0 +1,23 @@
+4. Aggregation Pipelines: Let the Server Do It For You
+"""You've used projection, sorting, indexing, and limits to speed up data fetching. 
+   But there are still annoying performance bottlenecks in your analysis pipelines. 
+   You still need to fetch a ton of data. Thus, network bandwidth and downstream processing and memory capacity still impact performance. 
+   This chapter is about using MongoDB to perform aggregations for you on the server."""
+
+4.1. Intro to Aggregation
+4.2. Sequencing stages
+4.3. Aggregating a few individuals' country data
+4.4. Passing the aggregation baton to Python
+4.5. Aggregation Operators and Grouping
+4.6. Field Paths and Sets
+4.7. Organizing prizes
+4.8. Gap years, aggregated
+4.9. Zoom into Array Fields
+4.10. Embedding aggregation expressions
+4.11. Here and elsewhere
+4.12. Countries of birth by prize category
+4.13. Something Extra: $addFields to Aid Analysis
+4.14. "...it's the life in your years"
+4.15. How many prizes were awarded to immigrants?
+4.16. Refinement: filter out "unaffiliated" people
+4.17. Wrap-Up
