355d755ef49f9dcc74301ab62ac0ea52c82face8
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-13 00:30:23 +0300

    Create introduction-to-databases.sql

---
+++ sql/introduction-to-databases.sql
@@ -0,0 +1,64 @@
+A Database
+      A database is a collection of data that 
+            is organized in a manner that facilitates ease of access, as well as efficient management and updating.
+      A database is made up of tables that store relevant information.
+      For example, you would use a database, if you were to create a website like YouTube, 
+            which contains a lot of information like videos, usernames, passwords, comments.contentImage
+      In this course we will learn how to create and query databases using SQL!
+      
+      |  Users  |  Videos  |  Comments  |
+        Table 1    Table 2    Table 3 
+        
+Database Tables
+      A table stores and displays data in a structured format consisting of columns and rows that are similar to those seen in Excel spreadsheets.
+      
+      Databases often contain multiple tables, each designed for a specific purpose. 
+      For example, imagine creating a database table of names and telephone numbers.
+
+      First, we would set up columns with the titles FirstName, LastName and TelephoneNumber.
+
+      Each table includes its own set of fields, based on the data it will store.
+      
+      A table has a specified number of columns but can have any number of rows.
+      
+      
+Primary Keys
+      A primary key is a field in the table that uniquely identifies the table records.
+
+      The primary key's main features:
+      - It must contain a unique value for each row.
+      - It cannot contain NULL values.
+
+      For example, our table contains a record for each name in a phone book. 
+      The unique ID number would be a good choice for a primary key in the table, 
+      as there is always the chance for more than one person to have the same name.
+                 
+      - Tables are limited to ONE primary key each.
+      - The primary key's value must be different for each row.
+
+
+      The unique column used to identify the records is called:
+          primary key
+
+
+
+What is SQL?
+      Once you understand what a database is, understanding SQL is easy. SQL stands for Structured Query Language.
+
+      SQL is used to access and manipulate a database.
+      MySQL is a program that understands SQL.
+
+      SQL can:
+      - insert, update, or delete records in a database.
+      - create new databases, table, stored procedures, views.
+      - retrieve data from a database, etc.
+      
+ 
+SQL is an ANSI (American National Standards Institute) standard, but there are different versions of the SQL language.
+Most SQL database programs have their own proprietary extensions in addition to the SQL standard, but all of them support the major commands.
+
+
+
+
+
+
