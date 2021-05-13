43a084c0e4150848abb3001a2c2c10d4f42a48b9
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-13 06:18:20 +0300

    Create module-1-quiz.py

---
+++ sql/module-1-quiz.py
@@ -0,0 +1,10 @@
+      to list the table names.
+SHOW TABLES;
+
+
+      Why use primary keys? To guarantee the uniqueness of a row
+
+      to select distinct names from the ''students'' table, ordered by name.
+SELECT DISTINCT name 
+FROM students
+ORDER BY name;  
