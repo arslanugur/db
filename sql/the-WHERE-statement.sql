a825a979b793cb21ac70f7ce47d2146fada7d8c0
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-13 17:15:39 +0300

    Create the-WHERE-statement.sql

---
+++ sql/the-WHERE-statement.sql
@@ -0,0 +1,27 @@
+--The WHERE Statement
+      The WHERE clause is used to extract only those records that fulfill a specified criterion.
+      The syntax for the WHERE clause:
+SELECT column_list 
+FROM table_name
+WHERE condition;
+
+      In a table, to SELECT a specific record:
+SELECT * FROM customers
+WHERE ID = 7;
+
+      The WHERE clause is used to extract only those records that fulfill a specified criterion.
+
+
+      to select the name of the student whose id is equal to 23.
+SELECT id, name 
+FROM students 
+WHERE id=23;
+
+
+
+
+
+
+
+
+      
