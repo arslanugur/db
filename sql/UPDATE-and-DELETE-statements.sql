5cb12ddcd98ba2a0c8d785440d08e36fa65656ce
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-15 18:52:12 +0300

    Update UPDATE-and-DELETE-statements.sql

--- sql/UPDATE-and-DELETE-statements.sql
+++ sql/UPDATE-and-DELETE-statements.sql
@@ -53,3 +53,37 @@ SELECT * from Employees;
       1	Robert	Smith	      5000
       
       You can specify the column order any way you like in the SET clause.
+      
+      
+      to update the ''name'' and ''age'' columns of the ''students'' table.
+UPDATE students SET
+name = 'Peter',
+age = 32
+WHERE id = 147;
+
+
+--Deleting Data
+      The DELETE statement is used to remove data from your table. DELETE queries work much like UPDATE queries.
+DELETE FROM table_name
+WHERE condition; 
+
+      For example, you can delete a specific employee from the table:
+DELETE FROM Employees
+WHERE ID=1;
+SELECT * from Employees;
+
+      Result:
+      id	firstname	lastname	salary
+      2	David	      Williams	1500
+      3	Chloe	      Anderson	3000
+      4	Emily	      Adams	      4500
+      
+      If you omit the WHERE clause, all records in the table will be deleted!
+      
+      The DELETE statement removes the data from the table permanently.
+      
+      
+       to delete a row from ''people'' in which the ids falls in the range of 5 to 10.
+DELETE FROM people
+WHERE id>5 AND id<10;
+
