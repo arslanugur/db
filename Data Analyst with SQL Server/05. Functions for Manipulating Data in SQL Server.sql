35ef0fec5f6881042a616aba53cddced1c80f711
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-15 19:53:06 +0300

    Update 05. Functions for Manipulating Data in SQL Server.sql

--- Data Analyst with SQL Server/05. Functions for Manipulating Data in SQL Server.sql
+++ Data Analyst with SQL Server/05. Functions for Manipulating Data in SQL Server.sql
@@ -1,127 +1,87 @@
+Course Description
+	In this course, you will learn how to make use of the most important functions for manipulating data provided by SQL Server. 
+	You can use these functions for processing and transforming data to get the results you want.
 
-# Types of character strings
-ALTER TABLE voters 
-ADD last_vote_date date;
+1. Choosing the appropriate data type
+	In this chapter, you will learn what are the most used data types in SQL Server. 
+	You will understand the differences between implicit and explicit conversion and how each type of conversion manifests. 
+	You will also get familiar with the functions used for explicitly converting data: CAST() and CONVERT().
 
-ALTER TABLE voters 
-ADD last_vote_time time;
-
-ALTER TABLE voters 
-ADD last_login datetime2;
-
-
-# Implicit conversion between data types
-SELECT first_name, last_name, total_votes FROM voters WHERE total_votes > 120
-
-
-# Data type precedence
-SELECT 
-	bean_type,
-	rating
-FROM ratings
-WHERE rating > 3;
+1.1. Welcome to this course!
+1.2. Working with different types of data
+1.3. Storing dates in a database
+1.4. Types of character strings
+1.5. Implicit conversion
+1.6. Implicit conversion between data types
+1.7. Data type precedence
+1.8. Explicit conversion
+1.9. CASTing data
+1.10. CONVERTing data
+1.11. Working with the correct data types
 
 
-# CASTing data
-SELECT 
-	-- Transform the year part from the birthdate to a string
-	first_name + ' ' + last_name + ' was born in ' + CAST(YEAR(birthdate) AS nvarchar) + '.' 
-FROM voters;
+2. Manipulating time
+	Date and time functions are an important topic for databases. 
+	In this chapter, you will get familiar with the most common functions for date and time manipulation. 
+	You will learn how to retrieve the current date, only parts from a date, 
+	to assemble a date from pieces and to check if an expression is a valid date or not.
 
-/////
-
-SELECT 
-	-- Transform to int the division of total_votes to 5.5
-	CAST(total_votes/5.5 AS int) AS DividedVotes
-FROM voters;
-
-/////
-
-SELECT 
-	first_name,
-	last_name,
-	total_votes
-FROM voters
--- Transform the total_votes to char of length 10
-WHERE CAST(total_votes AS char(10)) LIKE '5%';
-
-# CONVERTing data
-SELECT 
-	email,
-    -- Convert birthdate to varchar show it like: "Mon dd,yyyy" 
-    CONVERT(varchar, birthdate, 107) AS birthdate
-FROM voters;
-
-/////
-
-SELECT 
-	company,
-    bean_origin,
-    -- Convert the rating column to an integer
-    CONVERT(int, rating) AS rating
-FROM ratings;
-
-/////
-
-SELECT 
-	company,
-    bean_origin,
-    rating
-FROM ratings
--- Convert the rating to an integer before comparison
-WHERE CONVERT(int, rating) = 3;
+2.1. Functions that return system date and time
+2.2. Get the know the system date and time functions
+2.3. Selecting parts of the system's date and time
+2.4. Functions returning date and time parts
+2.5. Extracting parts from a date
+2.6. Generating descriptive date parts
+2.7. Presenting parts of a date
+2.8. Creating a date from parts
+2.9. Performing arithmetic operations on dates
+2.10. Arithmetic operations with dates
+2.11. Modifying the value of a date
+2.12. Calculating the difference between dates
+2.13. Validating if an expression is a date
+2.14. Changing the date format
+2.15. Changing the default language
+2.16. Correctly applying different date functions
 
 
-# Working with the correct data types
-SELECT 
-	first_name,
-	last_name,
-	gender,
-	country
-FROM voters
-WHERE country = 'Belgium'
-	-- Select only the female voters
-	AND gender='F'
-    -- Select only people who voted more than 20 times   
-    AND total_votes>20;
-    
-/////
+3. Working With Strings
+	Strings are one of the most commonly used data types in databases. 
+	It's important to know what you can do with them. 
+	In this chapter, you will learn how to manipulate strings, to get the results you want.
 
- SELECT 
-	first_name,
-    last_name,
-	-- Convert birthdate to varchar(10) and show it as yy/mm/dd. This format corresponds to value 11 of the "style" parameter.
-	CONVERT(varchar(10), birthdate, 11) AS birthdate,
-    gender,
-    country
-FROM voters
-WHERE country = 'Belgium' 
-    -- Select only the female voters
-	AND gender = 'F'
-    -- Select only people who voted more than 20 times  
-    AND total_votes > 20;
-
-/////
-
-SELECT
-	first_name,
-    last_name,
-	-- Convert birthdate to varchar(10) to show it as yy/mm/dd
-	CONVERT(varchar(10), birthdate, 11) AS birthdate,
-    gender,
-    country,
-    -- Convert the total_votes number to nvarchar
-    'Voted ' + CAST(total_votes AS nvarchar) + ' times.' AS comments
-FROM voters
-WHERE country = 'Belgium'
-    -- Select only the female voters
-	AND gender = 'F'
-    -- Select only people who voted more than 20 times
-    AND total_votes > 20;  
+3.1. Functions for positions
+3.2. Calculating the length of a string
+3.3. Looking for a string within a string
+3.4. Looking for a pattern within a string
+3.5. Functions for string transformation
+3.6. Changing to lowercase and uppercase
+3.7. Using the beginning or end of a string
+3.8. Extracting a substring
+3.9. Replacing parts of a string
+3.10. Functions manipulating groups of strings
+3.11. Concatenating data
+3.12. Aggregating strings
+3.13. Splitting a string into pieces
+3.14. Applying various string functions on data
 
 
+4. Recognizing Numeric Data Properties
+	In this chapter, you will work with functions applied to numeric data. 
+	You will use aggregate functions for calculating the minimum, maximum or the sum of values from a set. 
+	You will learn how to raise a number to a power or to calculate its square root.
 
-
-
-
+4.1. Aggregate arithmetic functions
+4.2. Learning how to count and add
+4.3. MINimizing and MAXimizing some results
+4.4. Analytic functions
+4.5. Accessing values from the next row
+4.6. Accessing values from the previous row
+4.7. Getting the first and last value
+4.8. Mathematical functions
+4.9. Extracting the sign and the absolute value
+4.10. Rounding numbers
+4.11. Working with exponential functions
+4.12. Manipulating numeric data
+4.13. Wrapping things up
+ 
 
