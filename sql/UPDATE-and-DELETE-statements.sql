6cecf7855562ace58329cc8e4e00b8e26e577fc6
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-15 17:52:53 +0300

    Create UPDATE-and-DELETE-statements.sql

---
+++ sql/UPDATE-and-DELETE-statements.sql
@@ -0,0 +1,34 @@
+--Updating Data
+      The UPDATE statement allows us to alter data in the table.
+
+      The basic syntax of an UPDATE query with a WHERE clause is as follows:
+UPDATE table_name
+SET column1=value1, column2=value2, ...
+WHERE condition;
+
+      You specify the column and its new value in a comma-separated list after the SET keyword.
+      If you omit the WHERE clause, all records in the table will be updated!
+      
+      Consider the following table called "Employees":
+      
+      id |FirstName |LastName |Salary
+      -----------------------------
+      1  |John      |Smiths	  |2000
+      2  |David     |Williams |1500
+      3  |Chloe     |Anderson |3000
+      4  |Emily     |Adams    |4500
+      
+      
+      To update John's salary, we can use the following query:
+UPDATE Employees 
+SET Salary=5000
+WHERE ID=1;
+SELECT * from Employees;
+
+      id	firstname	lastname	salary
+      2	  David	    Williams	1500
+      3	  Chloe	    Anderson	3000
+      4	  Emily	    Adams	    4500
+      1	  John	    Smith	    5000
+      
+      
